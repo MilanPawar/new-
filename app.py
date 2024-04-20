50ee4791ae026c469a8280cfce5846af606867ca
milan pawar <82013765+MilanPawar@users.noreply.github.com>
2024-04-20 07:27:43 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/', methods=['GET', 'POST'])
+def calculate_sum():
+    if request.method == 'POST':
+        try:
+            num1 = float(request.form['num1'])
+            num2 = float(request.form['num2'])
+            result = num1 + num2
+            return render_template('result.html', result=result)
+        except ValueError:
+            return "Invalid input. Please enter valid numbers."
+    return render_template('input_form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
